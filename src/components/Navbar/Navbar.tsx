83c1a54470ac177052274621901f118e60260b76
Rus7Iv <rusivary@gmail.com>
2024-01-13 17:48:49 +0400

    added Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+import { CommandBar } from '@fluentui/react';
+import { items } from './types/enum';
+
+const Navbar = () => {
+  return <CommandBar items={items} />;
+};
+
+export default Navbar;
---
+++ src/components/Navbar/types/enum.ts
@@ -0,0 +1,17 @@
+export const items = [
+    {
+      key: 'home',
+      text: 'Главная',
+      iconProps: { iconName: 'Home' },
+    },
+    {
+      key: 'products',
+      text: 'Товары',
+      iconProps: { iconName: 'Shop' },
+    },
+    {
+      key: 'cart',
+      text: 'Корзина',
+      iconProps: { iconName: 'ShoppingCart' },
+    },
+  ];

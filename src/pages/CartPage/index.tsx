fba13c1259099663a12e473a54a95e43faf65063
Rus7Iv <rusivary@gmail.com>
2024-01-19 16:11:25 +0400

    added ProductCard in CartPage

--- src/pages/CartPage/index.tsx
+++ src/pages/CartPage/index.tsx
@@ -1,25 +1,24 @@
 import { useContext } from 'react';
 import { CartContext } from './type/CartContex';
+import ProductCard from '../../components/ProductCard/ProductCard';
 import './styles.css'
 
 const Cart = () => {
-  const { cart } = useContext(CartContext);
+  const { cart, addToCart, removeFromCart } = useContext(CartContext); 
 
   return (
     <>
       <h1 className='cart-title'>Корзина</h1>
       <div className="cart">
         {cart.length > 0 ? (
-          cart.map((product, index) => (
-            <div key={index} className="fluent-card">
-              <div className="fluent-header">
-                <h2>{product.name}</h2>
-              </div>
-              <p>{product.description}</p>
-              <div className="fluent-footer">
-                <h3>Цена: {product.price} руб.</h3>
-              </div>
-            </div>
+          cart.map((product) => (
+            <ProductCard 
+              key={product.id} 
+              product={product} 
+              onAddToCart={addToCart} 
+              onRemoveFromCart={removeFromCart}
+              inCart={true}
+            />
           ))
         ) : (
           <div className='empty-cart-container'>
